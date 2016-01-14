8f9ca04e2751ecf3c6492a1714d85ed25ec5b522
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-14 15:11:35 +0000

    hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,31 @@
 import { Component } from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
+
+public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+};
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My Second Angular 2  App</h1>'
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <div><input value="{{ hero.name }}" placeholder="name"></div>
+      </div>
+      `
 })
-export class AppComponent {};
+
+export class AppComponent {
+    public title = "Tour of Heros";
+    public hero = Hero;
+};
